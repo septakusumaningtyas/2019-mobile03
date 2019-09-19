cdd6eb2edc58298ec6b4e7f084a6cac0e9916c43
Septa Kusumaningtyas <septakusumaningtyas@gmail.com>
2019-09-19 10:57:49 +0700

    Mengubah tampilan linier menjadi relative

---
+++ RelativeLayout/app/src/main/java/id/ac/polinema/relativelayout/LinearToRelative.java
@@ -0,0 +1,14 @@
+package id.ac.polinema.relativelayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LinearToRelative extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_linear_to_relative);
+    }
+}
